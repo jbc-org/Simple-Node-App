655ad905d2efe453c29dfef3ae292605c35918ec
Jonathan Cuningham <jbcuningham@gmail.com>
2022-10-22 07:44:50 -0500

    Clean up commented code

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const Eureka = require('eureka-js-client').Eureka;
-// const eurekaHost = (process.env.EUREKA_CLIENT_SERVICEURL_DEFAULTZONE || '127.0.0.1');
-// const eurekaPort = 8761;
-// const hostName = (process.env.HOSTNAME || 'localhost')
-// const ipAddr = '172.0.0.1';
 const eurekaHelper = require("./eurekaHelper").registerWithEureka;
 const app = express();
 
@@ -14,8 +10,6 @@ app.get("/", (req, res) => {
 
 
 app.listen(5000, () => {
-  // console.dir(eurekaHelper.registerWithEureka;
-  // eurekaHelper.registerWithEureka("myNodeApp", 5000);
   eurekaHelper("myNodeApp", 5000);
   console.log("listening on port: " + 5000);
 })
